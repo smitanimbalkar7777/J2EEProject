9717394b6b6b90319d3c165df7a10163d050fa89
smita suresh nimbalkar <118713974+smitanimbalkar7777@users.noreply.github.com>
2023-01-28 19:28:41 +0530

    Add files via upload

---
+++ musicplayer/musicplayer1.java/song.java
@@ -0,0 +1,58 @@
+package com.jspiders.musicplayer;
+
+public class song {
+	   int id;
+	   String songName;
+	   String singerName;
+	   String movieName;
+	   String composer;
+	   String lyrist;
+	   double duration;
+	  
+	   public int getId() {
+		   return id;
+	   }
+        public void setId(int id) {
+        	this.id=id;
+        }
+        public String getSongName() {
+        	return songName;
+        }
+
+public void setSongName(String songName) {
+	this.songName=songName;
+}
+ public String getSingerName() {
+	 return singerName;
+ }
+ public void setSingerName(String singerName) {
+	 this.singerName=singerName;
+ }
+ public String getmovieName() {
+	return movieName;
+ }
+ public void setmovieName(String movieName) {
+	 this.movieName=movieName;
+ }
+ public String getComposer() {
+	 return composer;
+ }
+ public void setComposer(String composer) {
+	 this.composer=composer;
+ }
+ public String getLyrist() {
+	 return lyrist;
+ }
+ public void setLyrist(String lyrist) {
+	 this.lyrist=lyrist;
+ }
+ public double getDuration() {
+	 return duration;
+ }
+ public void setDuration(double duration) {
+	 this.duration=duration;
+ }
+
+	 
+	 
+ }
